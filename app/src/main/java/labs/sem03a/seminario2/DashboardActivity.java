689a025222538a6fbbaf8ccb8e545f6f1e4b8d88
Guimonto <36004939+Guimonto@users.noreply.github.com>
2018-02-06 20:58:52 +0100

    About Activity

--- app/src/main/java/labs/sem03a/seminario2/DashboardActivity.java
+++ app/src/main/java/labs/sem03a/seminario2/DashboardActivity.java
@@ -1,7 +1,9 @@
 package labs.sem03a.seminario2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    public void predeterminado(View v){
+        Intent intent = new Intent();
     }
 }
