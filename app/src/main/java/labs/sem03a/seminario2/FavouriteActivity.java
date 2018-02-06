b261ce840130c2c9f157ce04841073183dfaa988
Guimonto <36004939+Guimonto@users.noreply.github.com>
2018-02-07 00:34:33 +0100

    Settings, Favourite

---
+++ app/src/main/java/labs/sem03a/seminario2/FavouriteActivity.java
@@ -0,0 +1,16 @@
+package labs.sem03a.seminario2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class FavouriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite);
+
+
+    }
+}
