e2449562189ee73281cf8b65f0acc13710ca09cd
guimonto <guimonto@gmail.com>
2018-02-07 18:39:55 +0100

    Lenguaje españo añadido

--- app/src/main/java/labs/sem03a/seminario2/QuotationActivity.java
+++ app/src/main/java/labs/sem03a/seminario2/QuotationActivity.java
@@ -3,6 +3,9 @@ package labs.sem03a.seminario2;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class QuotationActivity extends AppCompatActivity {
@@ -13,6 +16,21 @@ public class QuotationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quotation);
 
         TextView tv = findViewById(R.id.textViewScroll);
-        //tv.setText(R.string.nameless);
+        tv.setText(getString(R.string.hello, getString(R.string.nameless)));
+
+        ImageButton button = findViewById(R.id.imageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tv = findViewById(R.id.textViewQuot);
+                tv.setText(getString(R.string.sample_author));
+
+                TextView tv1 = findViewById(R.id.textViewScroll);
+                tv1.setText(getString(R.string.sample));
+            }
+        });
+
     }
+
+
 }
