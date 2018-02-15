2a887fe006b18f90bfa6c3f7af506b18380337ec
Guimonto <36004939+Guimonto@users.noreply.github.com>
2018-02-15 01:00:12 +0100

    menu_favourite y menu_quotations

--- app/src/main/java/labs/sem03a/seminario2/FavouriteActivity.java
+++ app/src/main/java/labs/sem03a/seminario2/FavouriteActivity.java
@@ -7,6 +7,8 @@ import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -92,6 +94,8 @@ public class FavouriteActivity extends AppCompatActivity {
         }
     }
 
+
+
     public void authorInfoImplicit(View v){
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
@@ -100,6 +104,49 @@ public class FavouriteActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_favourite, menu);
+
+        menu.findItem(R.id.clear_action).setVisible(clearAllQuotations);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch (item.getItemId()){
+
+            case R.id.clear_action:
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                builder.setMessage(R.string.confirmation_delete_all);
+
+                builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        quotationList.clear();
+
+                        ((CustomAdapter) adapter).notifyDataSetChanged();
+
+                        clearAllQuotations = false;
+
+                        supportInvalidateOptionsMenu();
+                    }
+                });
+
+                builder.setNegativeButton(android.R.string.no, null);
+
+                builder.create().show();
+
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
--- app/src/main/java/labs/sem03a/seminario2/QuotationActivity.java
+++ app/src/main/java/labs/sem03a/seminario2/QuotationActivity.java
@@ -3,12 +3,21 @@ package labs.sem03a.seminario2;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class QuotationActivity extends AppCompatActivity {
+
+    boolean newQuotation = true;
+
+    boolean addQuotation = false;
+
+    TextView tvCita;
+    TextView tvAuthor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +27,40 @@ public class QuotationActivity extends AppCompatActivity {
         TextView tv = findViewById(R.id.textViewScroll);
         tv.setText(getString(R.string.hello, getString(R.string.nameless)));
 
-        ImageButton button = findViewById(R.id.imageButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView tv = findViewById(R.id.textViewQuot);
-                tv.setText(getString(R.string.sample_author));
+        tvCita = findViewById(R.id.textViewScroll);
+        tvAuthor = findViewById(R.id.textViewQuot);
 
-                TextView tv1 = findViewById(R.id.textViewScroll);
-                tv1.setText(getString(R.string.sample));
-            }
-        });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_quotation, menu);
+
+        menu.findItem(R.id.refresh_button).setVisible(newQuotation);
+
+        menu.findItem(R.id.add_button).setVisible(newQuotation && addQuotation);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+
+            case R.id.refresh_button:
+
+                tvCita.setText(R.string.sample);
+                tvAuthor.setText(R.string.sample_author);
+
+                return true;
+
+            case R.id.add_button:
+
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
